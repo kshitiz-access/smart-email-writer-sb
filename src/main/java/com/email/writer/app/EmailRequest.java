70973e34a0903bf24c253f285858688b03a9c23c
kshitiz-access <pandey.kshitiz05@gmail.com>
2025-10-28 09:12:00 +0530

    Create EmailRequest model

---
+++ src/main/java/com/email/writer/app/EmailRequest.java
@@ -0,0 +1,17 @@
+package com.email.writer.app;
+
+import lombok.Data;
+
+@Data
+public class EmailRequest {
+    private String emailContent;
+    private String tone;
+    
+    public void setEmailContent(String emailContent) {
+        this.emailContent = emailContent;
+    }
+    
+    public void setTone(String tone) {
+        this.tone = tone;
+    }
+}
